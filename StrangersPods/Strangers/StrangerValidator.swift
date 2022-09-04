e6a58a76bd76466ca2ec9c6bfb3666e9c4e25dc6
Akanksha <>
2022-09-04 23:50:06 +0800

    Initial commit

---
+++ StrangersPods/Strangers/StrangerValidator.swift
@@ -0,0 +1,15 @@
+//
+//  StrangerValidator.swift
+//  Strangers
+//
+//  Created by Akanksha Thakur on 4/9/22.
+//
+
+import Foundation
+
+public struct Validator {
+  
+  public static func sayHello() {
+    print("Heyy Powerful girl!!")
+  }
+}
---
+++ StrangersPods/build/Strangers-iphoneos.xcarchive/Products/Library/Frameworks/Strangers.framework/Headers/Strangers.h
@@ -0,0 +1,18 @@
+//
+//  Strangers.h
+//  Strangers
+//
+//  Created by Akanksha Thakur on 4/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Strangers.
+FOUNDATION_EXPORT double StrangersVersionNumber;
+
+//! Project version string for Strangers.
+FOUNDATION_EXPORT const unsigned char StrangersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Strangers/PublicHeader.h>
+
+
